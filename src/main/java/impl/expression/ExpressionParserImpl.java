36944c226ce6c996d1359a7a33f2e7985e96bb33
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-05-17 23:33:35 +0300

    [task-2] Introduce ExpressionParser interface and tests for it's implementation

---
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -0,0 +1,12 @@
+package impl.expression;
+
+import api.expression.ExpressionParser;
+import api.expression.ParseException;
+
+public class ExpressionParserImpl implements ExpressionParser {
+    @Override
+    public int parse(String expression) throws ParseException {
+        // TODO: write implementation here
+        return 0;
+    }
+}
