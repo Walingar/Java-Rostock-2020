fdf4d51a6513887e3497fcd6430dfbefa163f538
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-05-17 23:11:04 +0300

    [task-1] Introduce interface which allows to apply convolution on images

---
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -0,0 +1,13 @@
+package impl.image;
+
+import api.image.ConvolutionProvider;
+
+import java.awt.*;
+
+public class ConvolutionProviderImpl implements ConvolutionProvider {
+    @Override
+    public Color[][] apply(Color[][] image, double[][] kernel) {
+        // TODO: write implementation here
+        return null;
+    }
+}
---
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -0,0 +1,19 @@
+package impl.image;
+
+import api.image.ImageConverter;
+
+import java.awt.*;
+
+public class ImageConverterImpl implements ImageConverter {
+    @Override
+    public Color[][] convertToColor(int[][] image) {
+        // TODO: write implementation here
+        return null;
+    }
+
+    @Override
+    public int[][] convertToRgb(Color[][] image) {
+        // TODO: write implementation here
+        return null;
+    }
+}
