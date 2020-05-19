bc21d2813249acf04b8a73c75695aaf7d8d0c638
Henning <henning.richter@uni-rostock.de>
2020-05-19 17:59:03 +0200

    type casts not working yet

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -9,15 +9,14 @@ public class ImageConverterImpl implements ImageConverter {
     @Override
     public Color[][] convertToColor(int[][] image) {
         // TODO: write implementation here
-        for(i=0, i<image.length, i++){ // do for every
-            for(j=0, j<image[i].length, j++){ // single pixel of image
-                // int consists of 4 bytes... split up and save seperately
-                image[i][j] = {
-                        r: image[i][j] >> 24 & 0xFF,
-                        g: image[i][j] >> 16 & 0xFF,
-                        b: image[i][j] >> 8 & 0xFF,
-                        a: image[i][j] & 0xFF
-                };
+        for(int i=0; i<image.length; i++){ // do for every
+            for(int j=0; j<image[i].length; j++){ // single pixel of image
+                // int consists of 4 bytes... split up and save seperately in same field
+                int r = image[i][j] >> 24 & 0xFF;
+                int g = image[i][j] >> 16 & 0xFF;
+                int b = image[i][j] >> 8 & 0xFF;
+                int a = image[i][j] & 0xFF;
+                image[i][j] = Color(r,g,b,a);
             }
         }
         return null;
@@ -26,14 +25,15 @@ public class ImageConverterImpl implements ImageConverter {
     @Override
     public int[][] convertToRgb(Color[][] image) {
         // TODO: write implementation here
-        for(i=0, i<image.length, i++){ // do for every
-            for(j=0, j<image[i].length, j++){ // single pixel of array
+        for(int i=0; i<image.length; i++){ // do for every
+            for(int j=0; j<image[i].length; j++){ // single pixel of image in same field
                 // int represented as 4 distinct bytes... re-combine
-                var r = image[i][j].getRed() & 0xFF;
-                var g = image[i][j].getGreen() & 0xFF;
-                var b = image[i][j].getBlue() & 0xFF;
-                var a = image[i][j].getAlpha() & 0xFF;
-                image[i][j] = (r << 24) + (g << 16) + (b << 8) + (a);
+                int r = image[i][j].getRed() & 0xFF;
+                int g = image[i][j].getGreen() & 0xFF;
+                int b = image[i][j].getBlue() & 0xFF;
+                int a = image[i][j].getAlpha() & 0xFF;
+                int rgba = (r << 24) + (g << 16) + (b << 8) + (a);
+                image[i][j] = (int) rgba;
             }
         }
         return null;
