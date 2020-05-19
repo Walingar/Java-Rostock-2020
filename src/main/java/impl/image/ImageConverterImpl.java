3ec8b8ee33e03e511a96d7ead55f7b96d081d7b1
Henning <henning.richter@uni-rostock.de>
2020-05-19 17:18:03 +0200

    attached comments

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -9,8 +9,8 @@ public class ImageConverterImpl implements ImageConverter {
     @Override
     public Color[][] convertToColor(int[][] image) {
         // TODO: write implementation here
-        for(i=0, i<image.length, i++){
-            for(j=0, j<image[i].length, j++){
+        for(i=0, i<image.length, i++){ // do for every
+            for(j=0, j<image[i].length, j++){ // single pixel of image
                 // int consists of 4 bytes... split up and save seperately
                 image[i][j] = {
                         r: image[i][j] >> 24 & 0xFF,
@@ -26,8 +26,8 @@ public class ImageConverterImpl implements ImageConverter {
     @Override
     public int[][] convertToRgb(Color[][] image) {
         // TODO: write implementation here
-        for(i=0, i<image.length, i++){
-            for(j=0, j<image[i].length, j++){
+        for(i=0, i<image.length, i++){ // do for every
+            for(j=0, j<image[i].length, j++){ // single pixel of array
                 // int represented as 4 distinct bytes... re-combine
                 var r = image[i][j].getRed() & 0xFF;
                 var g = image[i][j].getGreen() & 0xFF;
