c13c674d1cbfa9f0d501556d133a420ae5790b08
Henning <henning.richter@uni-rostock.de>
2020-05-19 18:33:37 +0200

    ok, almost the 2/3 are working... at least no error

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -9,22 +9,24 @@ public class ImageConverterImpl implements ImageConverter {
     @Override
     public Color[][] convertToColor(int[][] image) {
         // TODO: write implementation here
-        for(int i=0; i<image.length; i++){ // do for every
-            for(int j=0; j<image[i].length; j++){ // single pixel of image
+        Color[][] temp = new Color[image.length][image[0].length]; // new temp array for Color types
+        for(int i=0; i<image.length; i++) { // do for every
+            for (int j = 0; j < image[i].length; j++) { // single pixel of image
                 // int consists of 4 bytes... split up and save seperately in same field
                 int r = image[i][j] >> 24 & 0xFF;
                 int g = image[i][j] >> 16 & 0xFF;
                 int b = image[i][j] >> 8 & 0xFF;
                 int a = image[i][j] & 0xFF;
-                image[i][j] = Color(r,g,b,a);
+                temp[i][j] = new Color(r, g, b, a);
             }
         }
-        return null;
+        return temp;
     }
 
     @Override
     public int[][] convertToRgb(Color[][] image) {
         // TODO: write implementation here
+        int[][] temp = new int[image.length][image[0].length];
         for(int i=0; i<image.length; i++){ // do for every
             for(int j=0; j<image[i].length; j++){ // single pixel of image in same field
                 // int represented as 4 distinct bytes... re-combine
@@ -32,10 +34,9 @@ public class ImageConverterImpl implements ImageConverter {
                 int g = image[i][j].getGreen() & 0xFF;
                 int b = image[i][j].getBlue() & 0xFF;
                 int a = image[i][j].getAlpha() & 0xFF;
-                int rgba = (r << 24) + (g << 16) + (b << 8) + (a);
-                image[i][j] = (int) rgba;
+                temp[i][j] = (r << 24) + (g << 16) + (b << 8) + (a);
             }
         }
-        return null;
+        return temp;
     }
 }
