9d96203dc24eda2940ab2958a53c2470005ea671
Henning <henning.richter@uni-rostock.de>
2020-05-29 16:40:50 +0200

    WHAT DA F*CK IS WRONG D:

--- src/main/java/impl/image/ConvolutionProviderImpl.java
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -18,25 +18,36 @@ public class ConvolutionProviderImpl implements ConvolutionProvider {
         int a = 255;
         int color_default = (b) + (g << 8) + (r << 16) + (a << 24); // default color for out of reach core
 
+        double kernel_divisor = 0.0;
+        for(int i = 0; i < kernel.length; i++){ // calculate kernel divisor
+            for(int j = 0; j < kernel[i].length; j++){
+                kernel_divisor = kernel_divisor + kernel[i][j];
+            }
+        }
+
+        int kernel_navigator_row_i = (int)Math.floor(kernel.length/2.0); // navigator for rows in image
+        int kernel_navigator_column_j = (int)Math.floor(kernel[0].length/2.0); // navigator for columns in image
+
         double[][] output = new double[temp.length][temp[0].length];
-        for(int i_o = 0; i_o < output.length; i_o++) { // image array
-            for(int j_o = 0; j_o < output[i_o].length; j_o++) {
+        for(int i_o = 0; i_o < output.length; i_o++) { // image array with i_o image rows
+            for(int j_o = 0; j_o < output[i_o].length; j_o++) { // and j_o image columns
                 output[i_o][j_o] = 0;
 
-                for (int i_k = 0; i_k < kernel.length; i_k++) { // kernel array
-                    for (int j_k = 0; j_k < kernel[i_k].length; j_k++) {
-                        double help = 0;
-                        int x = i_k - (int)Math.floor(kernel.length/2.0); // navigate in image array due to kernel array (can be -1, 0, +1)
-                        int y = j_k - (int)Math.floor(kernel[i_k].length/2.0); // navigate in image array due to kernel array (can be -1, 0, +1)
-                        if( (i_o + x > -1) && (i_o + x < output.length) && (j_o + y > -1) && (j_o + y < output[i_o].length) ){ // core in image range
-                            help = (temp[i_o+x][j_o+y] * kernel[i_k][j_k]); // calculate as usual
+                for (int i_k = 0; i_k < kernel.length; i_k++) { // kernel array with i_k kernel rows
+                    for (int j_k = 0; j_k < kernel[i_k].length; j_k++) { // and j_k kernel columns
+                        double help;
+                        int n_x = i_k - kernel_navigator_row_i; // navigate in image array due to kernel array (can be -1, 0, +1 for 3x3 kernel)
+                        int n_y = j_k - kernel_navigator_column_j; // navigate in image array due to kernel array (can be -1, 0, +1 for 3x3 kernel)
+                        if( (i_o + n_x > -1) && (i_o + n_x < output.length) && (j_o + n_y > -1) && (j_o + n_y < output[i_o].length) ){ // core in image range
+                            help = (temp[i_o+n_x][j_o+n_y] * kernel[i_k][j_k]); // calculate as usual
                         } else{ // core out of image range
-                            help = color_default * kernel[i_k][j_k];
+                            help = color_default * kernel[i_k][j_k]; // calculate with default color
                         }
                         output[i_o][j_o] = output[i_o][j_o] + Math.round(help); // round after multiplication
                     }
                 }
 
+                output[i_o][j_o] = Math.round(output[i_o][j_o] / kernel_divisor); // apply kernel divisor to sum of kernel fields
             }
         }
 
