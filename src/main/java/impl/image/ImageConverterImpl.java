57620f50d0c4bf7c132020c21b4e6225165fa92a
Henning <henning.richter@uni-rostock.de>
2020-05-30 14:33:42 +0200

    [task 1: Henning]: To ImageConverterImpl: -Applied code conventions -Renamed variables -New: Used methods of class Color  instead

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -9,35 +9,28 @@ public class ImageConverterImpl implements ImageConverter {
     @Override
     public Color[][] convertToColor(int[][] image) {
 
-        Color[][] temp = new Color[image.length][image[0].length]; // new temp array for Color types
-        for(int i=0; i<image.length; i++) { // do for every
-            for (int j = 0; j < image[i].length; j++) { // single pixel of image
-                // int consists of 4 bytes... split up and save seperately in same field
-                int b = (image[i][j] >> 0) & 0xFF;
-                int g = (image[i][j] >> 8) & 0xFF;
-                int r = (image[i][j] >> 16) & 0xFF;
-                int a = (image[i][j] >> 24) & 0xFF;
-                temp[i][j] = new Color(r, g, b, a);
+        int imageSizeRow = image.length;
+        int imageSizeColumn = image[0].length;
+        Color[][] result = new Color[imageSizeRow][imageSizeColumn];
+        for (int imageRow = 0; imageRow < imageSizeRow; imageRow++) {
+            for (int imageColumn = 0; imageColumn < imageSizeColumn; imageColumn++) {
+                result[imageRow][imageColumn] = new Color(image[imageRow][imageColumn]);
             }
         }
-
-        return temp;
+        return result;
     }
 
     @Override
     public int[][] convertToRgb(Color[][] image) {
-        // TODO: write implementation here
-        int[][] temp = new int[image.length][image[0].length];
-        for(int i=0; i<image.length; i++){ // do for every
-            for(int j=0; j<image[i].length; j++){ // single pixel of image in same field
-                // int represented as 4 distinct bytes... re-combine
-                int r = image[i][j].getRed() & 0xFF;
-                int g = image[i][j].getGreen() & 0xFF;
-                int b = image[i][j].getBlue() & 0xFF;
-                int a = image[i][j].getAlpha() & 0xFF;
-                temp[i][j] = (b) + (g << 8) + (r << 16) + (a << 24);
+
+        int imageSizeRow = image.length;
+        int imageSizeColumn = image[0].length;
+        int[][] result = new int[imageSizeRow][imageSizeColumn];
+        for (int imageRow = 0; imageRow < imageSizeRow; imageRow++) {
+            for (int imageColumn = 0; imageColumn < imageSizeColumn; imageColumn++) {
+                result[imageRow][imageColumn] = image[imageRow][imageColumn].getRGB();
             }
         }
-        return temp;
+        return result;
     }
 }
