13df52262da0612375b9c89e7bbba93827178581
Tommy <tliefke_pay@web.de>
2020-06-01 19:30:11 +0200

    changed variable names for better understanding and style

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -8,73 +8,52 @@ public class ImageConverterImpl implements ImageConverter {
 
     @Override
     public Color[][] convertToColor(int[][] image) {
-        //TODO
-        Color[][] coloritem = new Color[image.length][image[0].length]; // new variable of Color-Class
+        Color[][] output = new Color[image.length][image[0].length];
 
-        for(int i=0; i<image.length; i++) { // do it for every line of pixels in the image
-            for (int j = 0; j < image[i].length; j++) { // do it for every column of pixels in the image
-                int b = image[i][j] & 0xFF;
-                int g = image[i][j] >> 8 & 0xFF;
-                int r = image[i][j] >> 16 & 0xFF;
-                int a = image[i][j] >> 24 & 0xFF;
-                coloritem[i][j] = new Color(r, g, b, a);
+        for (int rowCounter = 0; rowCounter < image.length; rowCounter++) { // do it for every line of pixels in the image
+            for (int columnCounter = 0; columnCounter < image[rowCounter].length; columnCounter++) { // do it for every column of pixels in the image
+                int blue = image[rowCounter][columnCounter] & 0xFF;
+                int green = image[rowCounter][columnCounter] >> 8 & 0xFF;
+                int red = image[rowCounter][columnCounter] >> 16 & 0xFF;
+                int alpha = image[rowCounter][columnCounter] >> 24 & 0xFF;
+                output[rowCounter][columnCounter] = new Color(red, green, blue, alpha);
             }
         }
-        return coloritem;
+        return output;
     }
 
     @Override
     public int[][] convertToRgb(Color[][] image) {
-        //TODO
-        int[][] colorarray = new int[image.length][image[0].length]; // new variable of int[][]
+        int[][] output = new int[image.length][image[0].length];
 
-        for(int i=0; i<image.length; i++){ // do it for every line of pixels in the image
-            for(int j=0; j<image[i].length; j++){ // do it for every column of pixels in the image
-                int r = image[i][j].getRed() & 0xFF; // & 0xFF(11111111) nessesary to cut of bit higher than 8?
-                int g = image[i][j].getGreen() & 0xFF;
-                int b = image[i][j].getBlue() & 0xFF;
-                int a = image[i][j].getAlpha() & 0xFF;
-                colorarray[i][j] = (b) + (g << 8) + (r << 16) + (a << 24);
+        for (int rowCounter = 0; rowCounter < image.length; rowCounter++) {
+            for (int columnCounter = 0; columnCounter < image[rowCounter].length; columnCounter++) {
+                int red = image[rowCounter][columnCounter].getRed() & 0xFF;
+                int green = image[rowCounter][columnCounter].getGreen() & 0xFF;
+                int blue = image[rowCounter][columnCounter].getBlue() & 0xFF;
+                int alpha = image[rowCounter][columnCounter].getAlpha() & 0xFF;
+                output[rowCounter][columnCounter] = (blue) + (green << 8) + (red << 16) + (alpha << 24);
+
+                /* second idea
+                output[rowCounter][columnCounter] = image[rowCounter][columnCounter].getRGB();
+                */
+
+                /* third idea
+                int red = image[rowCounter][columnCounter].getRed(); // int between 0-255
+                red = Integer.toBinaryString(red);
+                int green = image[rowCounter][columnCounter].getGreen(); // int between 0-255
+                green = Integer.toBinaryString(green);
+                int blue = image[rowCounter][columnCounter].getBlue(); // int between 0-255
+                blue = Integer.toBinaryString(blue);
+                int alpha = image[rowCounter][columnCounter].getAlpha(); // int between 0-255
+                alpha = Integer.toBinaryString(alpha);
+                output[rowCounter][columnCounter] = alpha + red + green + blue;
+                */
             }
         }
-        return colorarray;
+        return output;
     }
 
-    /* second idea
-    public int[][] convertToRgb(Color[][] image) {
-
-        int[][] colorarray = new int[image.length][image[0].length]; // new variable of int[][]
-
-        for(int i=0; i<image.length; i++){ // do for every
-            for(int j=0; j<image[i].length; j++){ // single pixel of image in same field
-                colorarray[i][j] = image[i][j].getRGB()
-            }
-        }
-        return colorarray;
-    }
-    */
-
-    /* third idea
-    public int[][] convertToRgb(Color[][] image) {
-
-        int[][] colorarray = new int[image.length][image[0].length]; // new variable of int[][]
-
-        for(int i=0; i<image.length; i++){ // do it for every line of pixels in the image
-            for(int j=0; j<image[i].length; j++){ // do it for every column of pixels in the image
-                int r = image[i][j].getRed(); // int between 0-255
-                Integer.toBinaryString(r);
-                int g = image[i][j].getGreen(); // int between 0-255
-                Integer.toBinaryString(g);
-                int b = image[i][j].getBlue(); // int between 0-255
-                Integer.toBinaryString(b);
-                int a = image[i][j].getAlpha(); // int between 0-255
-                Integer.toBinaryString(a);
-                colorarray[i][j] = a + r + g + b; // (b) + (g << 8) + (r << 16) + (a << 24)
-            }
-        }
-        return colorarray;
-    }
-    */
 
 }
 
