b434de32adcf0bd03ae4f4ae8a4257b10ef77fcf
Henning <henning.richter@uni-rostock.de>
2020-06-07 15:50:24 +0200

    [task 2: Henning]: Added: null handling, whitespace removal,parsing (exception when letter, build to int when number, calculate number when + || -) Why: Needed to solve the task Open cases: long handling, arithmetic exception

--- src/main/java/impl/expression/ExpressionParserImpl.java
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -6,7 +6,52 @@ import api.expression.ParseException;
 public class ExpressionParserImpl implements ExpressionParser {
     @Override
     public int parse(String expression) throws ParseException {
-        // TODO: write implementation here
-        return 0;
+
+        if (expression == null){
+            throw new IllegalArgumentException();
+        }
+
+        int result = 0;
+        String originalExpression = expression;
+        expression = expression.replaceAll("\\s+", ""); // remove whitespaces
+
+        int stringLength = expression.length();
+        String parsedInteger = "";
+        String parsedOperator = "";
+
+        for (int stringIndex = 0; stringIndex < stringLength; stringIndex++) {
+
+            char currentElement = expression.charAt(stringIndex);
+
+            if (Character.isLetter(currentElement)) {
+                throw new ParseException(originalExpression);
+            }
+
+            if (Character.isDigit(currentElement)) {
+                parsedInteger += currentElement;
+            }
+
+            if (!Character.isDigit(currentElement) || stringIndex == stringLength - 1) {
+                if (stringIndex == 0 && currentElement == '-'){
+                    parsedOperator = ""+currentElement;
+                } else {
+                    int parsedInt = Integer.parseInt(parsedInteger);
+                    if (parsedOperator.equals("")) {
+                        result = parsedInt;
+                    } else {
+                        if (parsedOperator.equals("+")) {
+                            result += parsedInt;
+                        } else {
+                            result -= parsedInt;
+                        }
+                    }
+                    parsedInteger = "";
+                    parsedOperator = "" + currentElement;
+                }
+            }
+        }
+
+        return result;
+
     }
 }
