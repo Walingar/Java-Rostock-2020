6d82c3a5193b0536a4176e92899b31fe791bf214
Tommy <tliefke_pay@web.de>
2020-06-13 23:26:04 +0200

    Add: edit missing variable cast caused by experimenting with the code, handle minus at the beginning of expression, expressions out of int-range, expressions out of int-range while calculating

--- src/main/java/impl/expression/ExpressionParserImpl.java
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -9,43 +9,70 @@ import java.util.List;
 public class ExpressionParserImpl implements ExpressionParser {
     @Override
     public int parse(String expression) throws ParseException {
-        int output = 0;
 
         if (expression == null) {
-             throw new IllegalArgumentException("expression is null");
-        }
-        else{
-            expression.replaceAll("\\s+",""); //removes all whitespaces in expression
-            int expressionLenght = expression.length();
-            List<Integer> numberList = new ArrayList<Integer>();
+            throw new IllegalArgumentException("expression is null");
+        } else {
+            expression = expression.replaceAll("\\s+", ""); //removes all whitespaces in expression
+            int expressionLength = expression.length();
+            String currentInt = "";
+            String currentOp = "";
 
-            for(int characterIndex=0; characterIndex<expressionLenght; characterIndex++){
-                char tempChar= expression.charAt(characterIndex);
+            int output = 0;
 
-                if ((tempChar >= 'a' && tempChar <= 'z') || (tempChar >= 'A' && tempChar <= 'Z')){
+            for (int characterIndex = 0; characterIndex < expressionLength; characterIndex++) {
+                char tempElement = expression.charAt(characterIndex);
+
+                // Case: char is letter
+                if (Character.isLetter(tempElement)) {
                     throw new ParseException("expression contains letters");
                 }
-                if (tempChar >= '0' && tempChar <= '9'){
-                    int number = tempChar;
-                    numberList.add(number);
 
+                // Case: char is digit
+                if (Character.isDigit(tempElement)) {
+                    currentInt += tempElement;
                 }
-                if (tempChar == '+'){
-                    if(numberList.isEmpty()==true){
 
+                // Case: char is +/- OR end of string
+                if (!Character.isDigit(tempElement) || characterIndex == expressionLength - 1) {
+
+                    //Case: - at the start of the expression
+                    if (tempElement == '-' && characterIndex == 0) {
+                        currentOp = "" + tempElement;
+                    } else {
+                        //Case: currentInt is out of Int-Range
+                        long check = Long.parseLong(currentInt);
+                        if (check > Integer.MAX_VALUE) {
+                            throw new ParseException("parsed number is out of Int-Range");
+                        }
+
+                        int parsedInt = Integer.parseInt(currentInt);
+                        if (currentOp == "") {
+                            output = parsedInt;
+                        } else {
+                            if (currentOp.equals("+")) {
+                                long checkOutput = output + parsedInt;
+                                if (checkOutput >= Integer.MAX_VALUE || checkOutput <= Integer.MIN_VALUE) {
+                                    throw new ArithmeticException("output out of Range");
+                                }
+                                output += parsedInt;
+                            } else if (currentOp.equals("-")) {
+                                long checkOutput = output - parsedInt;
+                                if (checkOutput >= Integer.MAX_VALUE || checkOutput <= Integer.MIN_VALUE) {
+                                    throw new ArithmeticException("output out of Range");
+                                }
+                                output -= parsedInt;
+                            }
+                        }
+                        currentInt = "";
+                        currentOp = "" + tempElement;
                     }
-                }
-                if (tempChar == '-') {
-
                 }
             }
 
-
-
-
-
+            return output;
         }
-
-        return output;
     }
+
+
 }
