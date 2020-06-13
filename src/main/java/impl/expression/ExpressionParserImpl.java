3f6cd9eb8b29f3b5fe607747bed065fa9d4af17a
Tommy <tliefke_pay@web.de>
2020-06-13 03:17:22 +0200

    In ExpressionParserImpl: added IllegalArgument Exception cause by null and ParseException caused by letters

--- src/main/java/impl/expression/ExpressionParserImpl.java
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -3,10 +3,49 @@ package impl.expression;
 import api.expression.ExpressionParser;
 import api.expression.ParseException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpressionParserImpl implements ExpressionParser {
     @Override
     public int parse(String expression) throws ParseException {
-        // TODO: write implementation here
-        return 0;
+        int output = 0;
+
+        if (expression == null) {
+             throw new IllegalArgumentException("expression is null");
+        }
+        else{
+            expression.replaceAll("\\s+",""); //removes all whitespaces in expression
+            int expressionLenght = expression.length();
+            List<Integer> numberList = new ArrayList<Integer>();
+
+            for(int characterIndex=0; characterIndex<expressionLenght; characterIndex++){
+                char tempChar= expression.charAt(characterIndex);
+
+                if ((tempChar >= 'a' && tempChar <= 'z') || (tempChar >= 'A' && tempChar <= 'Z')){
+                    throw new ParseException("expression contains letters");
+                }
+                if (tempChar >= '0' && tempChar <= '9'){
+                    int number = tempChar;
+                    numberList.add(number);
+
+                }
+                if (tempChar == '+'){
+                    if(numberList.isEmpty()==true){
+
+                    }
+                }
+                if (tempChar == '-') {
+
+                }
+            }
+
+
+
+
+
+        }
+
+        return output;
     }
 }
