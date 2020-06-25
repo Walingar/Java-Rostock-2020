dcd5c48d4f1b5c6b999314fbf18a2f28eb53c12a
Henning <henning.richter@uni-rostock.de>
2020-06-25 17:13:23 +0200

    Applied implementation of Interface using Array and LinkedList

--- src/main/java/impl/queue/ArrayQueueFactory.java
+++ src/main/java/impl/queue/ArrayQueueFactory.java
@@ -2,8 +2,66 @@ package impl.queue;
 
 import api.queue.IntQueue;
 
-public class ArrayQueueFactory {
+public class ArrayQueueFactory implements IntQueue{
+    private static int capacity;
+    private static Integer[] queueArray;
+
     public static IntQueue getInstance(int maxSize) {
-        return null;
+        capacity = maxSize;
+        queueArray = new Integer[capacity];
+        return new ArrayQueueFactory();
+    }
+
+    @Override
+    public void add(int e) {
+        int currentSize = getSize();
+        if (currentSize == capacity){
+            throw new IllegalStateException();
+        } else {
+            queueArray[currentSize] = e;
+        }
+    }
+
+    @Override
+    public Integer remove() {
+        int currentSize = getSize();
+        if (currentSize == 0) {
+            return null;
+        } else {
+            Integer output = queueArray[0];
+            for (int currentElement = 1; currentElement < capacity; currentElement++) {
+                int newPosition = currentElement - 1;
+                if (queueArray[currentElement] != null) {
+                    queueArray[newPosition] = queueArray[currentElement];
+                } else {
+                    queueArray[newPosition] = null;
+                    break;
+                }
+            }
+            return output;
+        }
+    }
+
+    @Override
+    public Integer element() {
+        int currentSize = getSize();
+        if (currentSize == 0) {
+            return null;
+        } else {
+            return queueArray[0];
+        }
+    }
+
+    @Override
+    public int getSize() {
+        int size = 0;
+        for (int currentElement = 0; currentElement < capacity; currentElement++) {
+            if (queueArray[currentElement] != null) {
+                size += 1;
+            } else {
+                break;
+            }
+        }
+        return size;
     }
 }
