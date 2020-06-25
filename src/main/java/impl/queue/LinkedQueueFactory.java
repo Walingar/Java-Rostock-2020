b58bbc15f90b2f24a871bd05b9986602cd6c9078
Henning <henning.richter@uni-rostock.de>
2020-06-25 17:13:23 +0200

    Applied implementation of Interface using Array and LinkedList

--- src/main/java/impl/queue/LinkedQueueFactory.java
+++ src/main/java/impl/queue/LinkedQueueFactory.java
@@ -1,9 +1,54 @@
 package impl.queue;
 
 import api.queue.IntQueue;
+import org.w3c.dom.Node;
 
-public class LinkedQueueFactory {
+import java.util.LinkedList;
+
+public class LinkedQueueFactory implements IntQueue{
+    private static int capacity;
+    private static LinkedList<Integer> queueLinkedList;
+
     public static IntQueue getInstance(int maxSize) {
-        return null;
+        capacity = maxSize;
+        queueLinkedList = new LinkedList<Integer>();
+        return new LinkedQueueFactory();
+    }
+
+    @Override
+    public void add(int e) {
+        int currentSize = getSize();
+        if (currentSize == capacity){
+            throw new IllegalStateException();
+        } else {
+            queueLinkedList.add(e);
+        }
+    }
+
+    @Override
+    public Integer remove() {
+        int currentSize = getSize();
+        if (currentSize == 0) {
+            return null;
+        } else {
+            Integer output = queueLinkedList.getFirst();
+            queueLinkedList.removeFirst();
+            return output;
+        }
+    }
+
+    @Override
+    public Integer element() {
+        int currentSize = getSize();
+        if (currentSize == 0) {
+            return null;
+        } else {
+            return queueLinkedList.getFirst();
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return queueLinkedList.size();
     }
 }
