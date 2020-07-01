167c9805a5849b4f31e2f027028b8c4566e6ccbc
Henning <henning.richter@uni-rostock.de>
2020-07-01 20:28:34 +0200

    Implemented YearTemperatureStatsFactory (completely passed) and almost DayTemperatureInfoParserFactory (implementation of DayTemperatureInfo is missing)

--- src/main/java/impl/weather/DayTemperatureInfoParserFactory.java
+++ src/main/java/impl/weather/DayTemperatureInfoParserFactory.java
@@ -1,9 +1,53 @@
 package impl.weather;
 
+import api.weather.DayTemperatureInfo;
 import api.weather.DayTemperatureInfoParser;
 
-public class DayTemperatureInfoParserFactory {
+import java.time.Month;
+
+public class DayTemperatureInfoParserFactory implements DayTemperatureInfoParser {
+
     public static DayTemperatureInfoParser getInstance() {
+        return new DayTemperatureInfoParserFactory();
+    }
+
+    @Override
+    public DayTemperatureInfo parse(String rawData) {
+
+        int stringLength = rawData.length();
+        String parsedDay = "";
+        String parsedMonth = "";
+        String parsedTemperature = "";
+        String parsedSeparator = "";
+        String parsedOperator = "";
+
+        for (int stringIndex = 0; stringIndex < stringLength; stringIndex++) {
+            char currentElement = rawData.charAt(stringIndex);
+            if (Character.isWhitespace(currentElement) || currentElement == '.') {
+                parsedSeparator += currentElement;
+            } else {
+                if (Character.isDigit(currentElement) && (parsedSeparator.length() == 0)) {
+                    parsedDay += currentElement;
+                }
+                if (Character.isDigit(currentElement) && (parsedSeparator.length() == 1)) {
+                    parsedMonth += currentElement;
+                }
+                if (Character.isDigit(currentElement) && (parsedSeparator.length() == 2)) {
+                    parsedTemperature += currentElement;
+                }
+                if (currentElement == '-' && (parsedSeparator.length() == 2)) {
+                    parsedOperator += currentElement;
+                }
+            }
+        }
+
+        int parsedDayInt = Integer.parseInt(parsedDay);
+        int parsedMonthInt = Integer.parseInt(parsedMonth);
+        Month month = Month.of(parsedMonthInt);
+        int parsedTemperatureInt = Integer.parseInt(parsedTemperature);
+        if (parsedOperator.length() != 0) {
+            parsedTemperatureInt *= -1;
+        }
         return null;
     }
 }
