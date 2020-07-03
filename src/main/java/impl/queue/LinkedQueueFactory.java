76bf81b694d1c774ef97ecf6479ba7d1e0f57181
Tommy <tliefke_pay@web.de>
2020-07-03 11:50:07 +0200

    Add: in ArrayQueueFactory the implementation of the queue and functions getInstance(), add(), remove(), element() and getSize()

--- src/main/java/impl/queue/LinkedQueueFactory.java
+++ src/main/java/impl/queue/LinkedQueueFactory.java
@@ -2,8 +2,31 @@ package impl.queue;
 
 import api.queue.IntQueue;
 
-public class LinkedQueueFactory {
+public class LinkedQueueFactory implements IntQueue{
+
     public static IntQueue getInstance(int maxSize) {
+        capacity = maxSize;
+        element = new QueueNode();
+        return new LinkedQueueFactory();
+    }
+
+    @Override
+    public void add(int e) {
+
+    }
+
+    @Override
+    public Integer remove() {
         return null;
+    }
+
+    @Override
+    public Integer element() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
     }
 }
