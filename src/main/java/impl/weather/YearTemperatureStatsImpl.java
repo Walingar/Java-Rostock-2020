a522b039e835184f10c85fd89b6338bc2b51e56b
Henning <henning.richter@uni-rostock.de>
2020-07-08 21:12:39 +0200

    Changed implementation in YearTemperatureStatsImpl from LinkedList with space for all month's days to Maps containing only known values

--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -4,87 +4,95 @@ import api.weather.DayTemperatureInfo;
 import api.weather.YearTemperatureStats;
 
 import java.time.Month;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
-    private List<List<DayTemperatureInfo>> yearTemperatures;
+    private Map<Month, Map<Integer, DayTemperatureInfo>> yearTemperatures;
+    private int monthCapacity;
 
     public YearTemperatureStatsImpl(){
-        yearTemperatures = new LinkedList<>();
-        for (Month month : Month.values()) {
-            List<DayTemperatureInfo> eachMonth = new LinkedList<>();
-            for (int day = 0; day < month.length(false); day++) eachMonth.add(null);
-            yearTemperatures.add(eachMonth);
+        monthCapacity = 0;
+        for (Month ignored : Month.values()) {
+            monthCapacity++;
         }
+        yearTemperatures = new HashMap<>(monthCapacity);
     }
+
 
     @Override
     public void updateStats(DayTemperatureInfo info) {
-        int dayIndex = info.getDay() - 1;
-        int monthIndex = info.getMonth().getValue() - 1;
-        yearTemperatures.get(monthIndex).set(dayIndex, info);
+        Month month = info.getMonth();
+        Integer day = info.getDay();
+        if (yearTemperatures.containsKey(month)) {
+            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
+            dayTemperatures.put(day, info);
+        } else {
+            Map<Integer, DayTemperatureInfo> dayTemperatures = new HashMap<>(month.length(false));
+            dayTemperatures.put(day, info);
+            yearTemperatures.put(month, dayTemperatures);
+        }
     }
 
     @Override
     public Double getAverageTemperature(Month month) {
-        int monthIndex = month.getValue() - 1;
-        List<DayTemperatureInfo> affectedMonth = yearTemperatures.get(monthIndex);
-        double sum = 0;
-        int daysKnown = 0;
-        int checkEmptyMonth = 0;
-        for (DayTemperatureInfo day : affectedMonth) {
-            if (day != null) {
+        if (yearTemperatures.containsKey(month)) {
+            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
+            Collection<DayTemperatureInfo> knownDays = dayTemperatures.values();
+            double sum = 0;
+            int knownDaysCount = knownDays.size();
+            for (DayTemperatureInfo day : knownDays) {
                 sum += day.getTemperature();
-                daysKnown += 1;
-            } else checkEmptyMonth += 1;
+            }
+            return sum / knownDaysCount;
+        } else {
+            return null;
         }
-        if (checkEmptyMonth == month.length(false)) return null;
-        else return sum / daysKnown;
     }
 
     @Override
     public Map<Month, Integer> getMaxTemperature() {
-        Map<Month, Integer> maxTemperatures = new HashMap<>();
-        for (List<DayTemperatureInfo> eachMonth : yearTemperatures) {
-            Month month = Month.of(yearTemperatures.indexOf(eachMonth) + 1);
+        Collection<Map<Integer, DayTemperatureInfo>> knownMonths = yearTemperatures.values();
+        Map<Month, Integer> output = new HashMap<>(monthCapacity);
+        for (Map<Integer, DayTemperatureInfo> eachMonth : knownMonths) {
+            Collection<DayTemperatureInfo> knownDays = eachMonth.values();
             Integer eachMax = null;
-            int checkEmptyMonth = 0;
-            for (DayTemperatureInfo day : eachMonth) {
-                if (day != null) {
-                    int dayTemperature = day.getTemperature();
-                    if (eachMax == null || eachMax < dayTemperature) eachMax = dayTemperature;
-                } else checkEmptyMonth += 1;
+            Month currentMonth = null;
+            for (DayTemperatureInfo eachDay : knownDays) {
+                int dayTemperature = eachDay.getTemperature();
+                if (eachMax == null || eachMax < dayTemperature) {
+                    eachMax = dayTemperature;
+                    currentMonth = eachDay.getMonth();
+                }
             }
-            if (checkEmptyMonth != month.length(false)) maxTemperatures.put(month, eachMax);
+            output.put(currentMonth, eachMax);
         }
-        return maxTemperatures;
+        return output;
     }
 
     @Override
     public List<DayTemperatureInfo> getSortedTemperature(Month month) {
-        List<DayTemperatureInfo> output = new LinkedList<>();
-        List<DayTemperatureInfo> affectedMonth = yearTemperatures.get(month.getValue() - 1);
-        int dayCount = 0;
-        for (DayTemperatureInfo currentDay : affectedMonth) {
-            if (currentDay != null) {
-                if (dayCount == 0) output.add(currentDay);
-                else {
+        List<DayTemperatureInfo> output = new ArrayList<>(month.length(false));
+        if (yearTemperatures.containsKey(month)) {
+            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
+            Collection<DayTemperatureInfo> knownDays = dayTemperatures.values();
+            int dayCount = 0;
+            for (DayTemperatureInfo eachDay : knownDays) {
+                if (dayCount == 0) {
+                    output.add(eachDay);
+                } else {
                     int previousDayIndex = dayCount - 1;
                     DayTemperatureInfo previousDay = output.get(previousDayIndex);
                     int previousTemperature = previousDay.getTemperature();
-                    int currentTemperature = currentDay.getTemperature();
-                    if (previousTemperature < currentTemperature) output.add(currentDay);
+                    int currentTemperature = eachDay.getTemperature();
+                    if (previousTemperature < currentTemperature) output.add(eachDay);
                     else {
                         while (previousTemperature > currentTemperature && previousDayIndex > 0) {
                             previousDayIndex--;
                             previousDay = output.get(previousDayIndex);
                             previousTemperature = previousDay.getTemperature();
                         }
-                        if (previousDayIndex != 0 || previousTemperature < currentTemperature) output.add(previousDayIndex + 1, currentDay);
-                        else output.add(previousDayIndex, currentDay);
+                        if (previousDayIndex != 0 || previousTemperature < currentTemperature) output.add(previousDayIndex + 1, eachDay);
+                        else output.add(previousDayIndex, eachDay);
                     }
                 }
                 dayCount++;
@@ -95,8 +103,12 @@ public class YearTemperatureStatsImpl implements YearTemperatureStats {
 
     @Override
     public DayTemperatureInfo getTemperature(int day, Month month) {
-        int dayIndex = day - 1;
-        int monthIndex = month.getValue() - 1;
-        return yearTemperatures.get(monthIndex).get(dayIndex);
+        if (yearTemperatures.containsKey(month)) {
+            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
+            if (dayTemperatures.containsKey(day)) {
+                return yearTemperatures.get(month).get(day);
+            }
+        }
+        return null;
     }
 }
