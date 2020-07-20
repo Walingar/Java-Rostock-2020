8c3a393a7221cb339bd1f1c1bb7f87b94e72fb78
Henning <henning.richter@uni-rostock.de>
2020-07-20 15:41:50 +0200

    Applied MonthInfo Class via Map<Month, MonthInfo>

--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -8,11 +8,11 @@ import java.util.*;
 
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
     private final Map<Month, Map<Integer, DayTemperatureInfo>> yearTemperatures;
-    private final Map<Month, Integer> maxTemperatures;
+    private final Map<Month, MonthInfo> monthInfo;
 
     public YearTemperatureStatsImpl() {
         yearTemperatures = new EnumMap<>(Month.class);
-        maxTemperatures = new EnumMap<>(Month.class);
+        monthInfo = new EnumMap<>(Month.class);
     }
 
     @Override
@@ -23,35 +23,41 @@ public class YearTemperatureStatsImpl implements YearTemperatureStats {
         if (yearTemperatures.containsKey(month)) {
             Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
             dayTemperatures.put(day, info);
-            if (maxTemperatures.get(month) < temperature) {
-                maxTemperatures.put(month, temperature);
+            MonthInfo currentMonthInfo = monthInfo.get(month);
+            if (currentMonthInfo.maxTemperature < temperature) {
+                currentMonthInfo.maxTemperature = temperature;
             }
+            int knownDays = currentMonthInfo.knownDays;
+            currentMonthInfo.averageTemperature = (knownDays * currentMonthInfo.averageTemperature + temperature) / (knownDays + 1);
+            currentMonthInfo.knownDays++;
         } else {
             Map<Integer, DayTemperatureInfo> dayTemperatures = new HashMap<>(month.length(false));
             dayTemperatures.put(day, info);
             yearTemperatures.put(month, dayTemperatures);
-            maxTemperatures.put(month, temperature);
+            MonthInfo newMonthInfo = new MonthInfo(month);
+            newMonthInfo.knownDays++;
+            newMonthInfo.maxTemperature = temperature;
+            newMonthInfo.averageTemperature = (double) temperature;
+            monthInfo.put(month, newMonthInfo);
         }
     }
 
     @Override
     public Double getAverageTemperature(Month month) {
-        if (!yearTemperatures.containsKey(month)) {
-            return null;
+        if (monthInfo.containsKey(month)) {
+            return monthInfo.get(month).averageTemperature;
         }
-        Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
-        Collection<DayTemperatureInfo> knownDays = dayTemperatures.values();
-        double sum = 0;
-        int knownDaysCount = knownDays.size();
-        for (DayTemperatureInfo day : knownDays) {
-            sum += day.getTemperature();
-        }
-        return sum / knownDaysCount;
+        return null;
     }
 
     @Override
     public Map<Month, Integer> getMaxTemperature() {
-        return maxTemperatures;
+        Map<Month, Integer> output = new EnumMap<>(Month.class);
+        Collection<MonthInfo> knownMonths = monthInfo.values();
+        for (MonthInfo eachMonth : knownMonths) {
+            output.put(eachMonth.month, eachMonth.maxTemperature);
+        }
+        return output;
     }
 
     @Override
@@ -124,6 +130,20 @@ public class YearTemperatureStatsImpl implements YearTemperatureStats {
         for (int i = restIndex; i < rest.size(); i++) {
             whole.set(wholeIndex, rest.get(i));
             wholeIndex++;
+        }
+    }
+
+    static class MonthInfo {
+        private Double averageTemperature;
+        private Integer maxTemperature;
+        private int knownDays;
+        private final Month month;
+
+        MonthInfo(Month thisMonth) {
+            averageTemperature = null;
+            maxTemperature = null;
+            knownDays = 0;
+            month = thisMonth;
         }
     }
 }
