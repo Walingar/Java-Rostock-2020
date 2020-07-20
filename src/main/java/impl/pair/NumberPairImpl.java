2198e8d0036bc960d8b7f596f42178dac8daa07d
Henning <henning.richter@uni-rostock.de>
2020-07-20 14:30:55 +0200

    NumberPairImpl now extends PairImpl

--- src/main/java/impl/pair/NumberPairImpl.java
+++ src/main/java/impl/pair/NumberPairImpl.java
@@ -2,47 +2,8 @@ package impl.pair;
 
 import api.pair.NumberPair;
 
-public class NumberPairImpl<T extends Number, K extends Number> implements NumberPair<T, K> {
-    private final T firstValue;
-    private final K secondValue;
-
+public class NumberPairImpl<T extends Number, K extends Number> extends PairImpl<T, K> implements NumberPair<T, K> {
     public NumberPairImpl(T first, K second) {
-        firstValue = first;
-        secondValue = second;
-    }
-
-    @Override
-    public T getFirst() {
-        return firstValue;
-    }
-
-    @Override
-    public K getSecond() {
-        return secondValue;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 17;
-        result = 31 * result + getFirst().hashCode();
-        result = 31 * result + getSecond().hashCode();
-        return result;
-    } // Idea from effective Java : Item 9
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof NumberPairImpl)) {
-            return false;
-        }
-        NumberPairImpl comparable = (NumberPairImpl) obj;
-        return comparable.getFirst().equals(firstValue) && comparable.getSecond().equals(secondValue);
-    }
-
-    @Override
-    public String toString() {
-        return "First: '" + firstValue + "', Second: '" + secondValue + "'";
+        super(first, second);
     }
 }
