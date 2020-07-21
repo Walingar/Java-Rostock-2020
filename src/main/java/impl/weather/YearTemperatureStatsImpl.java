3288119ad16344042807212f96bbe81694622fe9
Henning <henning.richter@uni-rostock.de>
2020-07-21 21:27:51 +0200

    Improved MonthInfo by extracting more code from YearTemperatureStatsImpl

--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -7,45 +7,24 @@ import java.time.Month;
 import java.util.*;
 
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
-    private final Map<Month, Map<Integer, DayTemperatureInfo>> yearTemperatures;
-    private final Map<Month, MonthInfo> monthInfo;
+    private final Map<Month, MonthInfo> monthInfoMap;
 
     public YearTemperatureStatsImpl() {
-        yearTemperatures = new EnumMap<>(Month.class);
-        monthInfo = new EnumMap<>(Month.class);
+        monthInfoMap = new EnumMap<>(Month.class);
     }
 
     @Override
     public void updateStats(DayTemperatureInfo info) {
         Month month = info.getMonth();
-        Integer day = info.getDay();
-        Integer temperature = info.getTemperature();
-        if (yearTemperatures.containsKey(month)) {
-            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
-            dayTemperatures.put(day, info);
-            MonthInfo currentMonthInfo = monthInfo.get(month);
-            if (currentMonthInfo.maxTemperature < temperature) {
-                currentMonthInfo.maxTemperature = temperature;
-            }
-            int knownDays = currentMonthInfo.knownDays;
-            currentMonthInfo.averageTemperature = (knownDays * currentMonthInfo.averageTemperature + temperature) / (knownDays + 1);
-            currentMonthInfo.knownDays++;
-        } else {
-            Map<Integer, DayTemperatureInfo> dayTemperatures = new LinkedHashMap<>(month.length(false));
-            dayTemperatures.put(day, info);
-            yearTemperatures.put(month, dayTemperatures);
-            MonthInfo newMonthInfo = new MonthInfo(month);
-            newMonthInfo.knownDays++;
-            newMonthInfo.maxTemperature = temperature;
-            newMonthInfo.averageTemperature = (double) temperature;
-            monthInfo.put(month, newMonthInfo);
-        }
+        monthInfoMap.putIfAbsent(month, new MonthInfo(month));
+        MonthInfo monthInfo = monthInfoMap.get(month);
+        monthInfo.updateMonthInfo(info);
     }
 
     @Override
     public Double getAverageTemperature(Month month) {
-        if (monthInfo.containsKey(month)) {
-            return monthInfo.get(month).averageTemperature;
+        if (monthInfoMap.containsKey(month)) {
+            return monthInfoMap.get(month).averageTemperature;
         }
         return null;
     }
@@ -53,7 +32,7 @@ public class YearTemperatureStatsImpl implements YearTemperatureStats {
     @Override
     public Map<Month, Integer> getMaxTemperature() {
         Map<Month, Integer> output = new EnumMap<>(Month.class);
-        Collection<MonthInfo> knownMonths = monthInfo.values();
+        Collection<MonthInfo> knownMonths = monthInfoMap.values();
         for (MonthInfo eachMonth : knownMonths) {
             output.put(eachMonth.month, eachMonth.maxTemperature);
         }
@@ -62,39 +41,61 @@ public class YearTemperatureStatsImpl implements YearTemperatureStats {
 
     @Override
     public List<DayTemperatureInfo> getSortedTemperature(Month month) {
-        if (yearTemperatures.containsKey(month)) {
-            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
-            Collection<DayTemperatureInfo> knownDays = dayTemperatures.values();
-            ArrayList<DayTemperatureInfo> output = new ArrayList<>(knownDays.size());
-            output.addAll(knownDays);
-            output.sort(Comparator.comparingInt(DayTemperatureInfo::getTemperature));
-            return output;
+        if (monthInfoMap.containsKey(month)) {
+            return monthInfoMap.get(month).getSortedTemperature();
         }
         return new ArrayList<>();
     }
 
     @Override
     public DayTemperatureInfo getTemperature(int day, Month month) {
-        if (yearTemperatures.containsKey(month)) {
-            Map<Integer, DayTemperatureInfo> dayTemperatures = yearTemperatures.get(month);
-            if (dayTemperatures.containsKey(day)) {
-                return yearTemperatures.get(month).get(day);
-            }
+        if (monthInfoMap.containsKey(month)) {
+            return monthInfoMap.get(month).getTemperature(day);
         }
         return null;
     }
 
-    static class MonthInfo {
+    private static class MonthInfo {
+        private final Month month;
+        private final Map<Integer, DayTemperatureInfo> temperatures;
         private Double averageTemperature;
         private Integer maxTemperature;
         private int knownDays;
-        private final Month month;
 
         MonthInfo(Month thisMonth) {
-            averageTemperature = null;
-            maxTemperature = null;
-            knownDays = 0;
             month = thisMonth;
+            temperatures = new LinkedHashMap<>(month.length(false));
+            knownDays = 0;
+        }
+
+        private void updateMonthInfo(DayTemperatureInfo info) {
+            Integer day = info.getDay();
+            temperatures.put(day, info);
+            int temperature = info.getTemperature();
+            if (maxTemperature == null || maxTemperature < temperature) {
+                maxTemperature = temperature;
+            }
+            if (averageTemperature == null) {
+                averageTemperature = (double) temperature;
+            } else {
+                averageTemperature = (knownDays * averageTemperature + temperature) / (knownDays + 1);
+            }
+            knownDays++;
+        }
+
+        private DayTemperatureInfo getTemperature(int day) {
+            if (temperatures.containsKey(day)) {
+                return temperatures.get(day);
+            }
+            return null;
+        }
+
+        private List<DayTemperatureInfo> getSortedTemperature() {
+            Collection<DayTemperatureInfo> knownDays = temperatures.values();
+            ArrayList<DayTemperatureInfo> output = new ArrayList<>(knownDays.size());
+            output.addAll(knownDays);
+            output.sort(Comparator.comparingInt(DayTemperatureInfo::getTemperature));
+            return output;
         }
     }
 }
