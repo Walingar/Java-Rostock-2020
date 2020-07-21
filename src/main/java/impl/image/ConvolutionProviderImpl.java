171578c10c240b95c007e03fd4393fde60dee12e
Tommy <tliefke_pay@web.de>
2020-07-22 00:12:42 +0200

    Changed apply() to good code style

--- src/main/java/impl/image/ConvolutionProviderImpl.java
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -8,26 +8,26 @@ import java.awt.*;
 public class ConvolutionProviderImpl implements ConvolutionProvider {
     @Override
     public Color[][] apply(Color[][] image, double[][] kernel) {
-        int kernelSizeRow = kernel.length;
-        int kernelSizeColumn = kernel[0].length;
+        int imageHeight = image.length;
+        int imageWidth = image[0].length;
 
-        int kernelRadiusRow = kernelSizeRow / 2;
-        int kernelRadiusColumn = kernelSizeColumn / 2;
+        int kernelHeight = kernel.length;
+        int kernelWidth = kernel[0].length;
 
-        int imageSizeRow = image.length;
-        int imageSizeColumn = image[0].length;
+        int kernelRadiusRow = kernelHeight / 2;
+        int kernelRadiusColumn = kernelWidth / 2;
 
-        Color[][] output = new Color[imageSizeRow][imageSizeColumn];
+        Color[][] output = new Color[imageHeight][imageWidth];
 
-        for (int imageRow = 0; imageRow < imageSizeRow; imageRow++) {
-            for (int imageColumn = 0; imageColumn < imageSizeColumn; imageColumn++) {
+        for (int imageRow = 0; imageRow < imageHeight; imageRow++) {
+            for (int imageColumn = 0; imageColumn < imageWidth; imageColumn++) {
 
                 int redOutput = 0;
                 int greenOutput = 0;
-                int blueOutput=0;
+                int blueOutput = 0;
 
-                for (int kernelRow = 0; kernelRow < kernelSizeRow; kernelRow++) {
-                    for (int kernelColumn = 0; kernelColumn < kernelSizeColumn; kernelColumn++) {
+                for (int kernelRow = 0; kernelRow < kernelHeight; kernelRow++) {
+                    for (int kernelColumn = 0; kernelColumn < kernelWidth; kernelColumn++) {
 
                         // shifts for navigation through kernel based on the selected pixel
                         int rowShift = kernelRow - kernelRadiusRow;
@@ -36,19 +36,22 @@ public class ConvolutionProviderImpl implements ConvolutionProvider {
                         int rowDisplacement = imageRow + rowShift;
                         int columnDisplacement = imageColumn + columnShift;
 
-                        // if kernelcoordinates based on the selected pixel fits is inside the image
-                        if ((rowDisplacement > -1) && (rowDisplacement < image.length) && (columnDisplacement > -1) && (columnDisplacement < image[imageRow].length)) {
-                            int redTemp = image[rowDisplacement][columnDisplacement].getRed();
-                            int greenTemp = image[rowDisplacement][columnDisplacement].getGreen();
-                            int blueTemp = image[rowDisplacement][columnDisplacement].getBlue();
+                        // if kernel coordinates based on the selected pixel fits is inside the image
+                        if ((rowDisplacement > -1) && (rowDisplacement < imageHeight) &&
+                                (columnDisplacement > -1) && (columnDisplacement < imageWidth)
+                        ) {
+                            Color imagePixel = image[rowDisplacement][columnDisplacement];
+                            int redTemp = imagePixel.getRed();
+                            int greenTemp = imagePixel.getGreen();
+                            int blueTemp = imagePixel.getBlue();
 
-                            redTemp = (int)Math.floor(redTemp * kernel[kernelRow][kernelColumn]);
-                            greenTemp = (int)Math.floor(greenTemp * kernel[kernelRow][kernelColumn]);
-                            blueTemp = (int)Math.floor(blueTemp * kernel[kernelRow][kernelColumn]);
+                            redTemp = (int) (redTemp * kernel[kernelRow][kernelColumn]);
+                            greenTemp = (int) (greenTemp * kernel[kernelRow][kernelColumn]);
+                            blueTemp = (int) (blueTemp * kernel[kernelRow][kernelColumn]);
 
-                            redOutput = redOutput + redTemp;
-                            greenOutput = greenOutput + greenTemp;
-                            blueOutput = blueOutput + blueTemp;
+                            redOutput += redTemp;
+                            greenOutput += greenTemp;
+                            blueOutput += blueTemp;
                         }
                     }
                 }
