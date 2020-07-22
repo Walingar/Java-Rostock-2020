4563fb7fe2dec1315cf7d82f25c5648f3a1c0fdc
Tommy <tliefke_pay@web.de>
2020-07-22 15:04:15 +0200

    Optimized ArrayQueueFactory and LinkedQueueFactory; implemented linear runtime for add() and getSize() in LinkedQueue

--- src/main/java/impl/queue/LinkedQueueFactory.java
+++ src/main/java/impl/queue/LinkedQueueFactory.java
@@ -5,8 +5,7 @@ import api.queue.IntQueue;
 public class LinkedQueueFactory {
 
     public static IntQueue getInstance(int maxSize) {
-        IntQueue lqueue = new LinkedQueue(maxSize);
-        return lqueue;
+        return new LinkedQueue(maxSize);
     }
 
 
