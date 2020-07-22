c990925a8789334276fc0e7c35400cd96a4e3673
Tommy <tliefke_pay@web.de>
2020-07-22 18:54:27 +0200

    Updated FileEncodingWriterImpl and FileEncodingReaderImpl

--- src/main/java/impl/file/FileEncodingWriterImpl.java
+++ src/main/java/impl/file/FileEncodingWriterImpl.java
@@ -16,15 +16,23 @@ public class FileEncodingWriterImpl implements FileEncodingWriter {
     @Override
     public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
         try {
+            if (!file.exists()) {
+                if (!file.getParentFile().mkdirs() || !file.createNewFile()){
+                    System.out.println("File not existing and not creatable");
+                    return;
+                }
+                // im confused by our lecture on Tuesday
+                // do i have to try to create a file in order to write into it? (when a.txt does not exists?)
+            }
+
             Reader reader = new InputStreamReader(data, dataEncoding);
-
-            OutputStream outputStream = new FileOutputStream(file);
+            OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file));
             Writer writer = new OutputStreamWriter(outputStream, fileEncoding);
-
             reader.transferTo(writer);
-            writer.flush();
+            writer.close();
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.toString());
         }
 
     }
