50f38eefdd3345d183cf6bcdfc52b77ec111601e
Tommy <tliefke_pay@web.de>
2020-07-25 20:10:15 +0200

    trying to create new file in FileEncodingWriterImpl

--- src/main/java/impl/file/FileEncodingWriterImpl.java
+++ src/main/java/impl/file/FileEncodingWriterImpl.java
@@ -1,7 +1,6 @@
 package impl.file;
 
 import api.file.FileEncodingWriter;
-import api.file.FileEncodingReader;
 
 import java.io.*;
 import java.nio.charset.Charset;
@@ -15,14 +14,19 @@ public class FileEncodingWriterImpl implements FileEncodingWriter {
 
     @Override
     public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
+
         try {
             if (!file.exists()) {
+
+                File newFile = new File("ab.txt");
+                if (newFile.createNewFile()) {
+                    System.out.println("File created: " + newFile.getName());
+                }
+
                 if (!file.getParentFile().mkdirs() || !file.createNewFile()){
                     System.out.println("File not existing and not creatable");
                     return;
                 }
-                // im confused by our lecture on Tuesday
-                // do i have to try to create a file in order to write into it? (when a.txt does not exists?)
             }
 
             Reader reader = new InputStreamReader(data, dataEncoding);
